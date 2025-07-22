1293dc8aec17d88cd908d129106330a533cefdbe
bhargavgurugubelli <gurugubellibhargav@gmail.com>
2025-07-22 19:55:05 +0530

    Initial commit with PostgreSQL config and project setup

--- env311/Scripts/activate.bat
+++ env311/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\gurug\OneDrive\Desktop\django-Saas-main\backend\env311
+set VIRTUAL_ENV=C:\Users\gurug\OneDrive\Desktop\quickbill-backend\env311
 
 if not defined PROMPT set PROMPT=$P$G
 
